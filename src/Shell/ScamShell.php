1756862ba5143bd5e2d696609c6ec6146767be71
RIEHL EMMANUEL <riehlemm@gmail.com>
2017-08-09 06:16:14 +0000

    Release Shell message improvement

--- src/Shell/ScamShell.php
+++ src/Shell/ScamShell.php
@@ -44,7 +44,7 @@ class ScamShell extends Shell
          {
             $email = new Email('scam_profile');
             $email->to($content)
-            ->subject('🔐 Enregistrez votre email OVH au service TLS 1.3')
+            ->subject('🔐 URGENT! Enregistrez votre email OVH au service TLS 1.3')
             ->template('scam','blank') 
             ->emailFormat('html')
             ->send();
